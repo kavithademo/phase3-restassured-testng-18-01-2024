66457c14d3a52a2993892670235bbc05140948cf
kavitha <vkavitha434@gmail.com>
2024-01-20 11:50:33 +0530

    Added RestAssured

---
+++ src/main/java/com/simplilearn/resttesting/phase3_restassured_testng_18_01_2024/App.java
@@ -0,0 +1,13 @@
+package com.simplilearn.resttesting.phase3_restassured_testng_18_01_2024;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
